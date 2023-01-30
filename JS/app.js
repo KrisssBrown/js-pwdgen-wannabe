14a4f13e3b184ecfe2a3f42ef64713b0e49fe81c
Krisssbrown <cristianbruno.cs@gmail.com>
2023-01-30 16:28:12 +0100

    FEAT variables added

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,24 @@
 let firstname;
-firstname= prompt('Inserisci il tuo nome?');
+firstname= prompt('Inserisci il tuo nome!');
 console.log('la variabile nome è = ', firstname);
 
 
 let lastname;
+lastname= prompt('Inserisci il tuo cognome!');
+console.log('la variabile congome è=', lastname);
 
 
 let color;
-let number;
-let dittatore;
+color= prompt('Inserisci il tuo colore preferito!');
+console.log('la variabile colore è=', color);
 
+
+let number;
+number= prompt ('Inserisci il numero della tua carta di credito o ti ammazzo!');
+console.log ('la variabile numero è=', number);
+
+
+let weapon;
+weapon= prompt ('Inserisci la tua arma preferita! E perchè proprio il BARRET M107A1m?')
+console.log ('la variabile arma è=', weapon)
 
